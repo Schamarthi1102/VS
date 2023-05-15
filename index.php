7e11f37815a6bdaf40dcd0ed882ee39260b5b50b
Schamarthi1102 <109301707+Schamarthi1102@users.noreply.github.com>
2023-05-15 18:34:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 </head>
 <body>
     <?php
+    
+    $allowedMethods = array(
+    'POST'
+);
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $first_name = $_POST["first_name"];
         $last_name = $_POST["last_name"];
